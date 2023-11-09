e36682c84a7288ae6b823e32efa434bbea874a4a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-08 22:39:14 -0300

    feat: download image

--- app/src/main/java/ar/edu/davinci/imagedownloader/ImageDownloader.java
+++ app/src/main/java/ar/edu/davinci/imagedownloader/ImageDownloader.java
@@ -1,15 +1,25 @@
 package ar.edu.davinci.imagedownloader;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class ImageDownloader extends AsyncTask<String, Integer, Bitmap> {
     private Bitmap imagenDescargada = null;
     @Override
     protected Bitmap doInBackground(String... strings) {
-        String URL = strings[0];
-        Log.i("testing", URL);
+        String URL_consulta = strings[0];
+        try {
+            URL consulta = new URL(URL_consulta); //preparar la consulta
+            InputStream contenido = (InputStream) consulta.getContent();
+            this.imagenDescargada = BitmapFactory.decodeStream(contenido);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return imagenDescargada;
     }
 }
